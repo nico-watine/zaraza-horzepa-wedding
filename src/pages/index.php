e87b4b1157b6bbe0da41807600dc61ae1301b1f0
Nico Watine <nico@studioncreations.com>
2016-12-21 14:27:25 -0600

    <aside> quote setup via inline css

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,15 @@ og-image:
 body-id: index
 body-class: index
 ---
+<header style="height: 60px; width: 100%; margin-bottom: 60px; background: rgba(0,0,0,0.35);">&nbsp;</header>
+<main>
+	<div class="row">
+		<div class="medium-6 medium-push-6 columns">
+			<aside style="text-align: right; background: rgba(0,0,0,0.35); width: 470px; padding: 12px 16px; float: right;">
+				<h1 style="color: #fff;">"I just met my future wife”</h1>
+				<p style="color: #fff; font-weight: 600; letter-spacing: .1px;"><i>- Joe Horzepa, on the phone with his best friend after meeting Juliana for the first time <br>
+				- June 13, 2000</i></p>
+			</aside>
+		</div>
+	</div>
+</main>
