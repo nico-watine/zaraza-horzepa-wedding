5959ccad69838f87641dfb5b887927aebdaf043a
Nico Watine <nico@studioncreations.com>
2016-12-22 12:49:42 -0600

    loveStory - move inline styles to SASS
    Currently at _index.scss file

--- src/pages/index.php
+++ src/pages/index.php
@@ -12,16 +12,16 @@ body-class: index
 <main>
 	<div class="row">
 		<div class="medium-6 medium-push-6 columns">
-			<aside style="text-align: right; background: rgba(0,0,0,0.35); width: 470px; padding: 12px 16px; float: right; margin-bottom: 60px;">
-				<h1 style="color: #fff;">"I just met my future wife”</h1>
-				<p style="color: #fff; font-weight: 600; letter-spacing: .1px;"><i>- Joe Horzepa, on the phone with his best friend after meeting Juliana for the first time <br>
+			<aside class="loveStory-headline">
+				<h1>"I just met my future wife”</h1>
+				<p><i>- Joe Horzepa, on the phone with his best friend after meeting Juliana for the first time <br>
 				- June 13, 2000</i></p>
 			</aside>
 		</div>
 	</div>
 	<div class="row">
 		<div class="large-7 large-push-5 columns">
-			<article style="background: #fff; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); padding: 20px;">
+			<article class="loveStory-prose">
 				<p>Joe and Juliana have a love story 16 years in the making. It all started one evening when Joe approached Juliana completely on his own. Juliana, busy chatting with her 5 girlfriends in Spanish, quickly dismissed Joe, thinking, “Who is this guy, and why won’t he just leave already?” Joe knew he just met his future wife, and he was not about to go anywhere.</p>
 				<p>For the next 10 years, on every June 13th, Joe made sure that Juliana received a massive bouquet of flowers. It did not matter if she was at home, or all the way in Colombia, visiting her mom. Those flowers always got to her. Finally, about a decade after that fateful introduction, Joe’s quiet patience and overt compassion got to Juliana’s heart, too.</p>
 				<p>On January 13, 2015, Joe proposed to Juliana at the restaurant Sorento’s. Sorento’s was their restaurant, where they would always meet up as friends (yet already married in Joe’s mind). He booked a private room, took her completely by surprise and got down on one knee to propose. The rest is history!</p>
