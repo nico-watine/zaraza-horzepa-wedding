2250b6e50989d9cd87dc856e50cb4e7c551c4ae5
Nico Watine <nico@studioncreations.com>
2017-01-13 13:21:38 -0600

    FINALLY off-canvas functioning, now style

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,61 +8,62 @@ og-image:
 body-id: index
 body-class: index
 ---
-<div class="off-canvas position-right" id="offCanvasMenu" data-off-canvas>
-    <!-- Close button -->
-    <button class="close-button" aria-label="Close menu" type="button" data-close>
-      <span aria-hidden="true">&times;</span>
-    </button>
+<div class="off-canvas position-left" id="offCanvas" data-off-canvas>
+	<!-- Close button -->
+	<button class="close-button" aria-label="Close menu" type="button" data-close>
+		<span aria-hidden="true">&times;</span>
+	</button>
 
-    <!-- Menu -->
-    <ul class="vertical menu">
-      <li><a href="#">Foundation</a></li>
-      <li><a href="#">Dot</a></li>
-      <li><a href="#">ZURB</a></li>
-      <li><a href="#">Com</a></li>
-      <li><a href="#">Slash</a></li>
-      <li><a href="#">Sites</a></li>
-    </ul>
+	<!-- Menu -->
+	<ul class="vertical menu">
+		<li><a href="#">Foundation</a></li>
+		<li><a href="#">Dot</a></li>
+		<li><a href="#">ZURB</a></li>
+		<li><a href="#">Com</a></li>
+		<li><a href="#">Slash</a></li>
+		<li><a href="#">Sites</a></li>
+	</ul>
 </div>
 <div class="title-bar">
-	<div class="title-bar-right">
-		<button class="menu-icon" type="button" data-open="offCanvasMenu"></button>
+	<div class="title-bar-left">
+		<button class="menu-icon" type="button" data-open="offCanvas"></button>
+		<!-- <button type="button" class="button" data-toggle="offCanvasLeft">Open Menu</button> -->
 	</div>
 </div>
 <div class="off-canvas-content" data-off-canvas-content style="background: none;">
-<header style="width: 100%; margin-bottom: 60px; background: rgba(0,0,0,0.35);">
-	<div class="row">
-		<div class="small-12 columns">
-			<nav class="mainNav text-right medium-text-left">
-				<ul class="menu">
-					<li class="active"><a href="/">Our Story</a></li>
-					<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
-					<li><a href="#">Wedding Details</a></li>
-					<li><a href="#">Cartagena Info</a></li>
-					<li><a href="#">Registry</a></li>
-				</ul>
-			</nav>
+	<header style="width: 100%; margin-bottom: 60px; background: rgba(0,0,0,0.35);">
+		<div class="row">
+			<div class="small-12 columns">
+				<nav class="mainNav text-right medium-text-left">
+					<ul class="menu">
+						<li class="active"><a href="/">Our Story</a></li>
+						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+						<li><a href="#">Wedding Details</a></li>
+						<li><a href="#">Cartagena Info</a></li>
+						<li><a href="#">Registry</a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
-	</div>
-</header>
-<main>
-	<div class="row">
-		<div class="medium-6 medium-push-6 columns">
-			<aside class="loveStory-headline">
-				<h1>"I just met my future wife”</h1>
-				<p><i>- Joe Horzepa, on the phone with his best friend after meeting Juliana for the first time <br>
-				- June 13, 2000</i></p>
-			</aside>
+	</header>
+	<main>
+		<div class="row">
+			<div class="medium-6 medium-push-6 columns">
+				<aside class="loveStory-headline">
+					<h1>"I just met my future wife”</h1>
+					<p><i>- Joe Horzepa, on the phone with his best friend after meeting Juliana for the first time <br>
+					- June 13, 2000</i></p>
+				</aside>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="medium-9 medium-push-3 large-7 large-push-5 columns">
-			<article class="loveStory-prose">
-				<p>Joe and Juliana have a love story sixteen years in the making. Their story started one hot and humid afternoon in Houston, Texas, on June 13, 2000, when Joe glanced down from his apartment window and noticed a group of women chatting and relaxing by the pool. One tall beautiful woman stood out from all the others and immediately captivated Joe. Joe made up his mind that he must meet this tall beautiful wonder. As he neared the group he could hear the women laughing and speaking to each other in Spanish. Not speaking any Spanish, Joe knew he had an uphill battle. Taking a deep breath, Joe looked straight at Juliana and addressed the group – “Hello, I’m Joe, can I interest anyone in drink?” Juliana listened, turned to her friends and quickly exchanged some comments in Spanish – comments that needed no translation - “Who is this guy, and why won’t he just leave us alone?” It was at that moment that both of their lives changed. Later that evening, Joe excitedly phoned a friend and informed him that he had just met his future wife – emphasis on “future” – it would prove to be a very long and rewarding journey up the hill.</p>
-				<p>For the next 10 years, on every June 13th, Joe ensured that Juliana received a little reminder of their first meeting. It did not matter if she was at home, or all the way in Colombia, visiting her family, flowers would always arrive on June 13th. The flowers led to the occasional dinner date at Sorrento’s restaurant, which led to regular conversations about life and politics, which led to a mutual respect and a deep and caring friendship. Finally, a decade after that fateful first step up the hill, Joe’s quiet patience and Juliana’s thoughtful compassion surprised both of them when they realized that slowly and thoughtfully each had been taking small loving steps towards each other.</p>
-				<p>On January 13, 2015, Joe proposed to Juliana at Sorrento’s opening a new and fulfilling chapter in their story. And so we ask you join us on another hot and humid afternoon in Cartagena to bear witness, celebrate and join in writing this new and exciting chapter in their story!</p>
-			</article>
+		<div class="row">
+			<div class="medium-9 medium-push-3 large-7 large-push-5 columns">
+				<article class="loveStory-prose">
+					<p>Joe and Juliana have a love story sixteen years in the making. Their story started one hot and humid afternoon in Houston, Texas, on June 13, 2000, when Joe glanced down from his apartment window and noticed a group of women chatting and relaxing by the pool. One tall beautiful woman stood out from all the others and immediately captivated Joe. Joe made up his mind that he must meet this tall beautiful wonder. As he neared the group he could hear the women laughing and speaking to each other in Spanish. Not speaking any Spanish, Joe knew he had an uphill battle. Taking a deep breath, Joe looked straight at Juliana and addressed the group – “Hello, I’m Joe, can I interest anyone in drink?” Juliana listened, turned to her friends and quickly exchanged some comments in Spanish – comments that needed no translation - “Who is this guy, and why won’t he just leave us alone?” It was at that moment that both of their lives changed. Later that evening, Joe excitedly phoned a friend and informed him that he had just met his future wife – emphasis on “future” – it would prove to be a very long and rewarding journey up the hill.</p>
+					<p>For the next 10 years, on every June 13th, Joe ensured that Juliana received a little reminder of their first meeting. It did not matter if she was at home, or all the way in Colombia, visiting her family, flowers would always arrive on June 13th. The flowers led to the occasional dinner date at Sorrento’s restaurant, which led to regular conversations about life and politics, which led to a mutual respect and a deep and caring friendship. Finally, a decade after that fateful first step up the hill, Joe’s quiet patience and Juliana’s thoughtful compassion surprised both of them when they realized that slowly and thoughtfully each had been taking small loving steps towards each other.</p>
+					<p>On January 13, 2015, Joe proposed to Juliana at Sorrento’s opening a new and fulfilling chapter in their story. And so we ask you join us on another hot and humid afternoon in Cartagena to bear witness, celebrate and join in writing this new and exciting chapter in their story!</p>
+				</article>
+			</div>
 		</div>
-	</div>
-</main>
+	</main>
 </div> <!-- end .off-canvas-content -->
