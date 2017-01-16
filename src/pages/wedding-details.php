79abb2c73bb325e9aa802959315bdecce9bfa17d
Nico Watine <nico@studioncreations.com>
2017-01-16 10:20:12 -0500

    Set up wedding-details with img and sass

---
+++ src/pages/wedding-details.php
@@ -0,0 +1,59 @@
+---
+url: /
+title: Our Story
+description: The beautiful love story of Joe & Juliana, 16 years in the making.
+og-title: Joe & Juliana's Love Story
+og-description: Joe knew from the beginning that he had just met his future wife. A love story 16 years in the making.
+og-image:
+body-id: index
+body-class: index
+---
+<div class="off-canvas position-right" id="offCanvas" data-transition="overlap" data-off-canvas>
+	<!-- Close button -->
+	<button class="close-button" aria-label="Close menu" type="button" data-close>
+		<span aria-hidden="true">&times;</span>
+	</button>
+
+	<!-- Menu -->
+	<ul class="vertical menu">
+		<li><a href="/">Our Story</a></li>
+		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+		<li class="active"><a href="#">Wedding Details</a></li>
+		<li><a href="#">Cartagena Info</a></li>
+		<li><a href="#">Registry</a></li>
+	</ul>
+</div>
+<div class="off-canvas-content" data-off-canvas-content style="background: none;">
+	<header>
+		<div class="row">
+		<button class="float-right menu-icon hide-for-medium" type="button" data-open="offCanvas"></button>
+			<div class="small-12 columns">
+				<nav class="mainNav text-right medium-text-left show-for-medium">
+					<ul class="menu">
+						<li><a href="/">Our Story</a></li>
+						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+						<li class="active"><a href="#">Wedding Details</a></li>
+						<li><a href="#">Cartagena Info</a></li>
+						<li><a href="#">Registry</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<main>
+		<div class="row">
+			<div class="medium-6 medium-push-6 columns">
+				<aside class="loveStory-headline">
+					<h1>Wedding Details</h1>
+				</aside>
+			</div>
+		</div>
+		<div class="row">
+			<div class="medium-9 medium-push-3 large-7 large-push-5 columns">
+				<article class="loveStory-prose" style="height: 500px;">
+					&nbsp;
+				</article>
+			</div>
+		</div>
+	</main>
+</div> <!-- end .off-canvas-content -->
