7b2d7c532cfe9b0a0e4f95368f39c9e5cd3808f6
Nico Watine <nico@studioncreations.com>
2017-01-19 17:13:26 -0600

    Registry page setup
    Go ahead and upload this one

---
+++ src/pages/registry.php
@@ -0,0 +1,68 @@
+---
+url: /
+title: Wedding Details
+description: 
+og-title: 
+og-description: 
+og-image:
+body-id: wedding-details
+body-class: wedding-details
+---
+<div class="off-canvas position-right" id="offCanvas" data-transition="overlap" data-off-canvas>
+	<!-- Close button -->
+	<button class="close-button" aria-label="Close menu" type="button" data-close>
+		<span aria-hidden="true">&times;</span>
+	</button>
+
+	<!-- Menu -->
+	<ul class="vertical menu">
+		<li><a href="/">Our Story</a></li>
+		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+		<li class="active"><a href="/wedding-details">Wedding Details</a></li>
+		<li><a href="/cartagena-info">Cartagena Info</a></li>
+		<li><a href="#">Registry</a></li>
+	</ul>
+</div>
+<div class="off-canvas-content" data-off-canvas-content style="background: none;">
+<div class="splash-background">
+	<header>
+		<div class="row">
+		<button class="float-right menu-icon hide-for-medium" type="button" data-open="offCanvas"></button>
+			<div class="small-12 columns">
+				<nav class="mainNav text-right medium-text-left show-for-medium">
+					<ul class="menu">
+						<li><a href="/">Our Story</a></li>
+						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+						<li class="active"><a href="/wedding-details">Wedding Details</a></li>
+						<li><a href="/cartagena-info">Cartagena Info</a></li>
+						<li><a href="#">Registry</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<!-- <main> -->
+		<div class="row">
+			<div class="medium-6 medium-push-6 columns">
+				<aside class="loveStory-headline">
+					<h1>Wedding Registry</h1>
+				</aside>
+			</div>
+		</div>
+</div>
+		<div class="row pageContent">
+			<article class="small-12 columns">
+				<div class="row weddingEvent" id="guestArrival">
+					<div class="small-12 columns">
+						<div class="row eventTitle">
+							<div class="small-12 columns">
+								<h2>Your presence at our wedding is the greatest gift of all.</h2>
+								<h5>However, should you wish to honor us with a gift, please donate to help treate and cure Angelman Syndrome. Donations may be made at <a href="http://cureangelman.org/donate/in-honor-of/" target="_blank">www.cureangelman.org/donate/in-honor-of/</a>. We ask that you donate in the name of Antonella Grace Woodall.</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+		</div>
+	<!-- </main> -->
+</div> <!-- end .off-canvas-content -->
