b68bade325630d4ed2895bc882aba1824206653c
Nico Watine <nico@studioncreations.com>
2017-01-19 15:34:22 -0600

    .weddingDay banner setup
    Now clean up the SASS

--- src/pages/wedding-details.php
+++ src/pages/wedding-details.php
@@ -50,7 +50,7 @@ body-class: wedding-details
 		</div>
 		<div class="row pageContent">
 			<article class="small-12 columns">
-				<div class="row weddingEvent" id="cocktailParty">
+				<div class="row weddingEvent" id="cocktailParty" style="margin-bottom: 40px;">
 					<div class="small-12 columns">
 						<div class="row eventTitle">
 							<div class="small-12 columns">
@@ -76,6 +76,36 @@ body-class: wedding-details
 								<div class="infoCard infoCard-theme text-center">
 									<h3>Theme</h3>
 									<p class="margin-0">White-Linen Party</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row weddingEvent" id="weddingDay" style="margin-bottom: 40px;">
+					<div class="small-12 columns">
+						<div class="row eventTitle">
+							<div class="small-12 columns">
+								<h2 style="padding-left: 50px; display: inline-block;">Cocktail Party</h2>
+								<h3 style="display: inline-block;">| FRIDAY, APRIL 28</h3>
+							</div>
+						</div>
+						<div class="row eventDetails">
+							<div class="large-4 columns">
+								<div class="infoCard infoCard-location text-center">
+									<h3>Location</h3>
+									<h5>Radisson Cartagena</h5>
+									<img src="/img/wedding-details/weddingDay/radisson-logo.png" alt="Radisson Cartagena Logo">
+									<p><a href="https://www.radisson.com/cartagena-hotel-co/cocartag" target="_blank">Official Website</a></p>
+								</div>
+							</div>
+							<div class="large-3 columns">
+								<div class="infoCard infoCard-time text-center" style="margin-bottom: 25px;">
+									<h3>Timeline</h3>
+									<p class="margin-0">5:30 - 6 | Ceremony <br>6:15 - 7 | Cocktail Hour <br>7 - 3 | Celebration Party</p>
+								</div>
+								<div class="infoCard infoCard-theme text-center">
+									<h3>Attire</h3>
+									<p class="margin-0">Women in red <br>Men in white</p>
 								</div>
 							</div>
 						</div>
