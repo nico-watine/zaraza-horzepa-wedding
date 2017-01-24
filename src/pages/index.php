8d8f893cf8487477ab0e9564814685d61e46da25
Nico Watine <nico@studioncreations.com>
2017-01-24 14:13:48 -0600

    Erased the whole “bg with box overlay”
    Newer, more simple layout with .splash-landing large on top. Also removed the box styles from the article

--- src/pages/index.php
+++ src/pages/index.php
@@ -24,6 +24,7 @@ body-class: index
 	</ul>
 </div>
 <div class="off-canvas-content" data-off-canvas-content style="background: none;">
+<div class="splash-background">
 	<header>
 		<div class="row">
 		<button class="float-right menu-icon hide-for-medium" type="button" data-open="offCanvas"></button>
@@ -40,7 +41,7 @@ body-class: index
 			</div>
 		</div>
 	</header>
-	<main>
+	<!-- <main> -->
 		<div class="row">
 			<div class="medium-6 medium-push-6 columns">
 				<aside class="loveStory-headline">
@@ -50,8 +51,9 @@ body-class: index
 				</aside>
 			</div>
 		</div>
+</div>
 		<div class="row">
-			<div class="medium-9 medium-push-3 large-7 large-push-5 columns">
+			<div class="medium-10 medium-push-1 columns end">
 				<article class="loveStory-prose">
 					<p>Joe and Juliana have a love story sixteen years in the making. Their story started one hot and humid afternoon in Houston, Texas, on June 13, 2000, when Joe glanced down from his apartment window and noticed a group of women chatting and relaxing by the pool. One tall beautiful woman stood out from all the others and immediately captivated Joe. Joe made up his mind that he must meet this tall beautiful wonder. As he neared the group he could hear the women laughing and speaking to each other in Spanish. Not speaking any Spanish, Joe knew he had an uphill battle. Taking a deep breath, Joe looked straight at Juliana and addressed the group – “Hello, I’m Joe, can I interest anyone in drink?” Juliana listened, turned to her friends and quickly exchanged some comments in Spanish – comments that needed no translation - “Who is this guy, and why won’t he just leave us alone?” It was at that moment that both of their lives changed. Later that evening, Joe excitedly phoned a friend and informed him that he had just met his future wife – emphasis on “future” – it would prove to be a very long and rewarding journey up the hill.</p>
 					<p>For the next 10 years, on every June 13th, Joe ensured that Juliana received a little reminder of their first meeting. It did not matter if she was at home, or all the way in Colombia, visiting her family, flowers would always arrive on June 13th. The flowers led to the occasional dinner date at Sorrento’s restaurant, which led to regular conversations about life and politics, which led to a mutual respect and a deep and caring friendship. Finally, a decade after that fateful first step up the hill, Joe’s quiet patience and Juliana’s thoughtful compassion surprised both of them when they realized that slowly and thoughtfully each had been taking small loving steps towards each other.</p>
@@ -59,5 +61,5 @@ body-class: index
 				</article>
 			</div>
 		</div>
-	</main>
+	<!-- </main> -->
 </div> <!-- end .off-canvas-content -->
