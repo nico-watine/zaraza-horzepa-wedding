d4b8ee7838ea9cc05d17cc3e50d9266a43a5d63e
Nico Watine <nico@studioncreations.com>
2017-01-25 19:56:20 -0600

    Our Story esp page created
    Now link it up

--- src/pages/esp/index.php
+++ src/pages/esp/index.php
@@ -8,35 +8,99 @@ og-image:
 body-id: index
 body-class: index
 ---
-<header style="width: 100%; margin-bottom: 60px; background: rgba(0,0,0,0.35);">
-	<div class="row">
-		<div class="small-12 columns">
-			<nav class="mainNav text-right medium-text-left">
-				<a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a>
-			</nav>
+<div class="off-canvas position-right" id="offCanvas" data-transition="overlap" data-off-canvas>
+	<!-- Close button -->
+	<button class="close-button" aria-label="Close menu" type="button" data-close>
+		<span aria-hidden="true">&times;</span>
+	</button>
+
+	<!-- Menu -->
+	<ul class="vertical menu">
+		<li class="active"><a href="/">Our Story</a></li>
+		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+		<li><a href="/wedding-details">Wedding Details</a></li>
+		<li><a href="/cartagena-info">Cartagena Info</a></li>
+		<li><a href="/registry">Registry</a></li>
+	</ul>
+</div>
+<div class="off-canvas-content" data-off-canvas-content style="background: none;">
+<div class="splash-background">
+	<header>
+		<div class="row">
+		<button class="float-right menu-icon hide-for-large" type="button" data-open="offCanvas"></button>
+			<ul class="menu languageSwitches float-left hide-for-large">
+				<li><a href="#"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
+				<li><a href="#"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
+			</ul>
+			<div class="small-12 columns">
+				<ul class="menu languageSwitches float-left show-for-large">
+					<li><a href="#"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
+					<li><a href="#"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
+				</ul>
+				<nav class="mainNav text-right medium-text-left show-for-large">
+					<ul class="menu float-right">
+						<li class="active"><a href="/">Our Story</a></li>
+						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+						<li><a href="/wedding-details">Wedding Details</a></li>
+						<li><a href="/cartagena-info">Cartagena Info</a></li>
+						<li><a href="/registry">Registry</a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
-	</div>
-</header>
-<main>
-	<div class="row">
-		<div class="medium-6 medium-push-6 columns">
-			<aside class="loveStory-headline">
-				<h1>"Acabo de conocer a mi futura esposa"</h1>
-				<p><i>- Joe Horzepa, le dijo a su mejor amigo por teléfono después de conocer a Juliana por primera vez <br>
-				- El 13 de Junio del año 2000</i></p>
-			</aside>
+	</header>
+	<!-- <main> -->
+		<div class="row">
+			<div class="medium-6 medium-push-6 columns">
+				<aside class="loveStory-headline">
+					<h1>"Acabo de conocer a mi futura esposa"</h1>
+					<p><i>- Joe Horzepa, le dijo a su mejor amigo por teléfono después de conocer a Juliana por primera vez <br>
+					- El 13 de Junio del año 2000</i></p>
+				</aside>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="medium-9 medium-push-3 large-7 large-push-5 columns">
-			<article class="loveStory-prose">
-				<p>Joe y Juliana tienen una historia de amor de dieciséis años. Su historia comenzó una tarde calurosa y húmeda en Houston, Texas, el 13 de Junio del año 2000, cuando Joe miró por la ventana de su apartamento y notó un grupo de mujeres charlando y relajándose en la piscina. Una mujer hermosa y alta se destacó de todas las demás e inmediatamente su belleza cautivó a Joe.</p>
-				<p>El decidió que debía conocer a esta hermosa mujer y al acercarse al grupo, pudo oír a las mujeres riéndose y hablando entre sí, en español. Sin el hablar este idioma, Joe sabía que tenía una difícil batalla. Respirando profundo, miró directamente a Juliana y se dirigió al grupo - "Hola, soy Joe, y les pregunto ¿será que alguien quiere una cerveza?" Juliana escuchó, se volvió a sus amigas y rápidamente intercambió algunos comentarios en español - comentarios que no necesitaban Traducción: "¿Quién es este tipo, y por qué no nos deja solas?" Fue en ese momento que ambas vidas cambiaron.</p>
-				<p>Más tarde esa noche, Joe con entusiasmo llamó a un amigo y le informó que acababa de conocer a su futura esposa - énfasis en el "futuro" - Ya que resultaría ser un viaje muy largo hasta que ambos pudieran tomar la decisión de estar juntos.</p>
-				<p>Durante los siguientes 10 años, cada 13 de junio, Joe se aseguró que Juliana recibiera un pequeño recordatorio de su primera reunión. No importaba si ella estaba en casa, la oficina, en Colombia visitando a su familia o donde estuviera... las flores llegarían siempre el 13 de junio (aquel grandioso 13 de junio).</p>
-				<p>Las flores llevaron a una cena casual en un restaurante llamado Sorrento, seguido por conversaciones sobre la vida y la política, lo que llevó a un respeto mutuo y una amistad profunda y cariñosa. Finalmente, una década después de aquel trascendente primer paso, la paciencia y determinación de Joe sorprendieron a Juliana. Ya que cuando se dieron cuenta, lentamente y cuidadosamente, cada uno había dado pequeños pasos amorosos … el uno hacia el otro.</p>
-				<p>El 13 de enero de 2015, Joe le propuso a Juliana, en Sorrento, la apertura de un nuevo y maravilloso capítulo en su historia. Así que les pedimos que se unan a nosotros en otra tarde calurosa, hermosa y llena de amor. En este paraíso, Cartagena, para dar testimonio, celebrar y unirse a la redacción de este nuevo y excitante capítulo en esta grandiosa historia de amor.</p>
-			</article>
+</div>
+		<div class="row">
+			<div class="medium-10 medium-push-1 columns end">
+				<article class="loveStory-prose">
+					<p>Joe y Juliana tienen una historia de amor de dieciséis años. Su historia comenzó una tarde calurosa y húmeda en Houston, Texas, el 13 de Junio del año 2000, cuando Joe miró por la ventana de su apartamento y notó un grupo de mujeres charlando y relajándose en la piscina. Una mujer hermosa y alta se destacó de todas las demás e inmediatamente su belleza cautivó a Joe.</p>
+					<p>El decidió que debía conocer a esta hermosa mujer y al acercarse al grupo, pudo oír a las mujeres riéndose y hablando entre sí, en español. Sin el hablar este idioma, Joe sabía que tenía una difícil batalla. Respirando profundo, miró directamente a Juliana y se dirigió al grupo - "Hola, soy Joe, y les pregunto ¿será que alguien quiere una cerveza?" Juliana escuchó, se volvió a sus amigas y rápidamente intercambió algunos comentarios en español - comentarios que no necesitaban Traducción: "¿Quién es este tipo, y por qué no nos deja solas?" Fue en ese momento que ambas vidas cambiaron.</p>
+					<p>Más tarde esa noche, Joe con entusiasmo llamó a un amigo y le informó que acababa de conocer a su futura esposa - énfasis en el "futuro" - Ya que resultaría ser un viaje muy largo hasta que ambos pudieran tomar la decisión de estar juntos.</p>
+					<p>Durante los siguientes 10 años, cada 13 de junio, Joe se aseguró que Juliana recibiera un pequeño recordatorio de su primera reunión. No importaba si ella estaba en casa, la oficina, en Colombia visitando a su familia o donde estuviera... las flores llegarían siempre el 13 de junio (aquel grandioso 13 de junio).</p>
+					<p>Las flores llevaron a una cena casual en un restaurante llamado Sorrento, seguido por conversaciones sobre la vida y la política, lo que llevó a un respeto mutuo y una amistad profunda y cariñosa. Finalmente, una década después de aquel trascendente primer paso, la paciencia y determinación de Joe sorprendieron a Juliana. Ya que cuando se dieron cuenta, lentamente y cuidadosamente, cada uno había dado pequeños pasos amorosos … el uno hacia el otro.</p>
+					<p>El 13 de enero de 2015, Joe le propuso a Juliana, en Sorrento, la apertura de un nuevo y maravilloso capítulo en su historia. Así que les pedimos que se unan a nosotros en otra tarde calurosa, hermosa y llena de amor. En este paraíso, Cartagena, para dar testimonio, celebrar y unirse a la redacción de este nuevo y excitante capítulo en esta grandiosa historia de amor.</p>
+				</article>
+			</div>
 		</div>
-	</div>
-</main>
+		<section class="image-gallery">
+			<div class="row">
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-1.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-2.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-3.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-4.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-5.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-6.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-7.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-8.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+			</div>
+		</section>
+	<!-- </main> -->
+</div> <!-- end .off-canvas-content -->
