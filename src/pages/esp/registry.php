047bf2cedba9d0f4b251df4f8223e936d214ae92
Nico Watine <nico@studioncreations.com>
2017-01-25 20:32:13 -0600

    Registry translations begun

--- src/pages/esp/registry.php
+++ src/pages/esp/registry.php
@@ -28,13 +28,13 @@ body-class: registry
 		<div class="row">
 		<button class="float-right menu-icon hide-for-large" type="button" data-open="offCanvas"></button>
 			<ul class="menu languageSwitches float-left hide-for-large">
-				<li><a href="/registry"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
-				<li><a href="/esp/registry"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
+				<li><a href="#"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
+				<li><a href="#"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
 			</ul>
 			<div class="small-12 columns">
 				<ul class="menu languageSwitches float-left show-for-large">
-				<li><a href="/registry"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
-				<li><a href="/esp/registry"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
+					<li><a href="#"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
+					<li><a href="#"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
 				</ul>
 				<nav class="mainNav text-right medium-text-left show-for-large">
 					<ul class="menu float-right">
