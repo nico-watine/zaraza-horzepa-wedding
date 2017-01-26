199102a76c253d0dbccf88376eab63242fae428f
Nico Watine <nico@studioncreations.com>
2017-01-25 20:29:20 -0600

    All in-page translations done
    For now merge, need translation for sunday

--- src/pages/esp/wedding-details.php
+++ src/pages/esp/wedding-details.php
@@ -53,7 +53,7 @@ body-class: wedding-details
 		<div class="row">
 			<div class="medium-6 medium-push-6 columns">
 				<aside class="pageTitle">
-					<h1>Wedding Details</h1>
+					<h1>Detalles de la Boda</h1>
 				</aside>
 			</div>
 		</div>
@@ -64,8 +64,8 @@ body-class: wedding-details
 					<div class="small-12 columns">
 						<div class="row eventTitle">
 							<div class="small-12 columns">
-								<h2>Guest Arrival</h2>
-								<h3><span class="hide-for-small-only">&nbsp;|</span> THURSDAY, APRIL 27</h3>
+								<h2>Llegada de Invitados</h2>
+								<h3><span class="hide-for-small-only">&nbsp;|</span> Jueves, ABRIL 27</h3>
 							</div>
 						</div>
 					</div>
@@ -77,8 +77,8 @@ body-class: wedding-details
 					<div class="small-12 columns">
 						<div class="row eventTitle">
 							<div class="small-12 columns">
-								<h2>Cocktail Party</h2>
-								<h3><span class="hide-for-small-only">&nbsp;|</span> FRIDAY, APRIL 28</h3>
+								<h2>Cocktail</h2>
+								<h3><span class="hide-for-small-only">&nbsp;|</span> Viernes, ABRIL 28</h3>
 							</div>
 						</div>
 						<div class="row hide-for-medium">
@@ -87,20 +87,20 @@ body-class: wedding-details
 						<div class="row eventDetails">
 							<div class="large-4 columns">
 								<div class="infoCard infoCard-location text-center">
-									<h3>Location</h3>
+									<h3>Lugar</h3>
 									<h5>Bastión Luxury Hotel</h5>
 									<img src="/img/wedding-details/cocktailParty/bastion-logo.png" alt="Bastion Luxury Hotel Logo">
-									<p><a href="http://en.bastionluxuryhotel.com" target="_blank">Official Website</a></p>
+									<p><a href="http://en.bastionluxuryhotel.com" target="_blank">Website</a></p>
 								</div>
 							</div>
 							<div class="large-3 columns text-center">
 								<div class="infoCard infoCard-time text-center">
-									<h3>Time</h3>
+									<h3>Hora</h3>
 									<p class="margin-0">6PM - 10PM</p>
 								</div>
 								<div class="infoCard infoCard-theme text-center">
 									<h3>Attire</h3>
-									<p class="margin-0">White-Linen Party</p>
+									<p class="margin-0">Fiesta de Blanco</p>
 								</div>
 							</div>
 						</div>
@@ -113,8 +113,8 @@ body-class: wedding-details
 					<div class="small-12 columns">
 						<div class="row eventTitle">
 							<div class="small-12 columns">
-								<h2>Wedding Day</h2>
-								<h3><span class="hide-for-small-only">&nbsp;|</span> Saturday, April 29</h3>
+								<h2>Dia de la Boda</h2>
+								<h3><span class="hide-for-small-only">&nbsp;|</span> Sabado, ABRIL 29</h3>
 							</div>
 						</div>
 						<div class="row hide-for-medium">
@@ -123,20 +123,20 @@ body-class: wedding-details
 						<div class="row eventDetails">
 							<div class="large-4 columns">
 								<div class="infoCard infoCard-location text-center">
-									<h3>Location</h3>
+									<h3>Lugar</h3>
 									<h5>Radisson Cartagena</h5>
 									<img src="/img/wedding-details/weddingDay/radisson-logo.png" alt="Radisson Cartagena Logo">
-									<p><a href="https://www.radisson.com/cartagena-hotel-co/cocartag" target="_blank">Official Website</a></p>
+									<p><a href="https://www.radisson.com/cartagena-hotel-co/cocartag" target="_blank">Website</a></p>
 								</div>
 							</div>
 							<div class="large-3 columns">
 								<div class="infoCard infoCard-time text-center" style="margin-bottom: 25px;">
-									<h3>Timeline</h3>
-									<p class="margin-0">5:30 - 6 | Ceremony <br>6:15 - 7 | Cocktail Hour <br>7 - 3 | Celebration Party</p>
+									<h3>Horario</h3>
+									<p class="margin-0">5:30 - 6 | Ceremonia <br>6:15 - 7 | Cocktail <br>7 - 3 | Fiesta</p>
 								</div>
 								<div class="infoCard infoCard-theme text-center">
 									<h3>Attire</h3>
-									<p class="margin-0">Ladies in red <br>Gentlemen in white</p>
+									<p class="margin-0">Damas en rojo <br>Caballeros en blanco</p>
 								</div>
 							</div>
 						</div>
@@ -149,7 +149,7 @@ body-class: wedding-details
 					<div class="small-12 columns">
 						<div class="row eventTitle">
 							<div class="small-12 columns">
-								<h3>Sunday, April 30</h3>
+								<h3>Domingo, ABRIL 30</h3>
 							</div>
 						</div>
 						<div class="row hide-for-medium">
