75e3f882fc56124d18266f92fc5947074ce9270a
Nico Watine <nico@studioncreations.com>
2017-01-25 18:58:22 -0600

    Page HTML scrapped
    Recreated markup with inline styes
    The page is good, now SASS it up

--- src/pages/registry.php
+++ src/pages/registry.php
@@ -24,7 +24,6 @@ body-class: registry
 	</ul>
 </div>
 <div class="off-canvas-content" data-off-canvas-content style="background: none;">
-<div class="splash-background">
 	<header>
 		<div class="row">
 		<button class="float-right menu-icon hide-for-medium" type="button" data-open="offCanvas"></button>
@@ -41,28 +40,17 @@ body-class: registry
 			</div>
 		</div>
 	</header>
-	<!-- <main> -->
-		<div class="row">
-			<div class="medium-6 medium-push-6 columns">
-				<aside class="loveStory-headline">
-					<h1>Wedding Registry</h1>
-				</aside>
-			</div>
+	<div class="row">
+		<div class="medium-6 medium-push-6 columns">
+			<aside class="loveStory-headline">
+				<h1>Wedding Registry</h1>
+			</aside>
 		</div>
+	</div>
+	<div class="row">
+		<article class="small-10 small-push-1 columns text-center" style="padding: 32px 16px; margin-bottom: 40px;background: #FEFEFE; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);">
+			<h2>Your presence at our wedding is the greatest <span class="inline-block">gift of all.</span></h2>
+			<h5>However, should you wish to honor us with a gift, please donate to help treate and cure Angelman Syndrome. Donations may be made at <a href="http://cureangelman.org/donate/in-honor-of/" target="_blank">www.cureangelman.org/donate/in-honor-of/</a>. We ask that you donate in the name of <span class="inline-block">Antonella Grace Woodall.</span></h5>
+		</article>
+	</div>
 </div>
-		<div class="row pageContent">
-			<article class="small-12 columns">
-				<div class="row weddingEvent" id="guestArrival">
-					<div class="small-12 columns">
-						<div class="row eventTitle">
-							<div class="small-12 columns">
-								<h2>Your presence at our wedding is the greatest gift of all.</h2>
-								<h5>However, should you wish to honor us with a gift, please donate to help treate and cure Angelman Syndrome. Donations may be made at <a href="http://cureangelman.org/donate/in-honor-of/" target="_blank">www.cureangelman.org/donate/in-honor-of/</a>. We ask that you donate in the name of Antonella Grace Woodall.</h5>
-							</div>
-						</div>
-					</div>
-				</div>
-			</article>
-		</div>
-	<!-- </main> -->
-</div> <!-- end .off-canvas-content -->
