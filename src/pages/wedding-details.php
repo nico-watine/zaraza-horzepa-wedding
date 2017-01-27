fecef3981b2578aa57d5eaeaab2a418ff6ac5bda
Nico Watine <nico@studioncreations.com>
2017-01-27 15:26:39 -0600

    scriptStyle added to wedding-details header

--- src/pages/wedding-details.php
+++ src/pages/wedding-details.php
@@ -52,7 +52,7 @@ body-class: wedding-details
 	<!-- <main> -->
 		<div class="row">
 			<div class="medium-6 medium-push-6 columns">
-				<aside class="pageTitle">
+				<aside class="pageTitle" id="scriptStyle">
 					<h1>Wedding Details</h1>
 				</aside>
 			</div>
