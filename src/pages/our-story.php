bd13f1a912431cd93a9d74e0271c0024c88f5966
Nico Watine <nico@studioncreations.com>
2017-02-01 17:42:35 -0600

    Create new base pages for our-story

---
+++ src/pages/our-story.php
@@ -0,0 +1,103 @@
+---
+url: /
+title: Our Story
+description: The beautiful love story of Joe & Juliana, 16 years in the making.
+og-title: Joe & Juliana's Love Story
+og-description: Joe knew from the beginning that he had just met his future wife. A love story 16 years in the making.
+og-image:
+body-id: index
+body-class: index
+---
+<div class="off-canvas position-right" id="offCanvas" data-transition="overlap" data-off-canvas>
+	<!-- Close button -->
+	<button class="close-button" aria-label="Close menu" type="button" data-close>
+		<span aria-hidden="true">&times;</span>
+	</button>
+
+	<!-- Menu -->
+	<ul class="vertical menu">
+		<li class="active"><a href="/">Our Story</a></li>
+		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+		<li><a href="/wedding-details">Wedding Details</a></li>
+		<li><a href="/cartagena-info">Cartagena Info</a></li>
+		<li><a href="/registry">Registry</a></li>
+	</ul>
+</div>
+<div class="off-canvas-content" data-off-canvas-content style="background: none;">
+<div class="splash-background">
+	<header>
+		<div class="row">
+		<button class="float-right menu-icon hide-for-large" type="button" data-open="offCanvas"></button>
+			<ul class="menu languageSwitches float-left hide-for-large">
+				<li><a href="/"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
+				<li><a href="/esp/"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
+			</ul>
+			<div class="small-12 columns">
+				<ul class="menu languageSwitches float-left show-for-large">
+					<li><a href="/"><img srcset="/img/icons/eng_2x.png 2x" src="/img/icons/eng.png" alt="English"></a></li>
+					<li><a href="/esp/"><img srcset="/img/icons/esp_2x.png 2x" src="/img/icons/esp.png" alt="Spanish"></a></li>
+				</ul>
+				<nav class="mainNav text-right medium-text-left show-for-large">
+					<ul class="menu float-right">
+						<li class="active"><a href="/">Our Story</a></li>
+						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+						<li><a href="/wedding-details">Wedding Details</a></li>
+						<li><a href="/cartagena-info">Cartagena Info</a></li>
+						<li><a href="/registry">Registry</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<!-- <main> -->
+		<div class="row">
+			<div class="medium-6 medium-push-6 columns">
+				<aside class="pageTitle">
+					<h1 class="script">"I just met my future wife”</h1>
+					<p><i>- Joe Horzepa, on the phone with his best friend after meeting Juliana for the first time <br>
+					- June 13, 2000</i></p>
+				</aside>
+			</div>
+		</div>
+</div>
+		<div class="row">
+			<div class="medium-10 medium-push-1 columns end">
+				<article>
+					<p>Joe and Juliana have a love story sixteen years in the making. Their story started one hot and humid afternoon in Houston, Texas, on June 13, 2000, when Joe glanced down from his apartment window and noticed a group of women chatting and relaxing by the pool. One tall beautiful woman stood out from all the others and immediately captivated Joe. Joe made up his mind that he must meet this tall beautiful wonder. As he neared the group he could hear the women laughing and speaking to each other in Spanish. Not speaking any Spanish, Joe knew he had an uphill battle. Taking a deep breath, Joe looked straight at Juliana and addressed the group – “Hello, I’m Joe, can I interest anyone in drink?” Juliana listened, turned to her friends and quickly exchanged some comments in Spanish – comments that needed no translation - “Who is this guy, and why won’t he just leave us alone?” It was at that moment that both of their lives changed. Later that evening, Joe excitedly phoned a friend and informed him that he had just met his future wife – emphasis on “future” – it would prove to be a very long and rewarding journey up the hill.</p>
+					<p>For the next 10 years, on every June 13th, Joe ensured that Juliana received a little reminder of their first meeting. It did not matter if she was at home, or all the way in Colombia, visiting her family, flowers would always arrive on June 13th. The flowers led to the occasional dinner date at Sorrento’s restaurant, which led to regular conversations about life and politics, which led to a mutual respect and a deep and caring friendship. Finally, a decade after that fateful first step up the hill, Joe’s quiet patience and Juliana’s thoughtful compassion surprised both of them when they realized that slowly and thoughtfully each had been taking small loving steps towards each other.</p>
+					<p>On January 13, 2015, Joe proposed to Juliana at Sorrento’s opening a new and fulfilling chapter in their story. And so we ask you join us on another hot and humid afternoon in Cartagena to bear witness, celebrate and join in writing this new and exciting chapter in their story!</p>
+				</article>
+			</div>
+		</div>
+		<section class="image-gallery">
+			<div class="row">
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-1.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-2.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-3.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-4.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-5.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-6.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-7.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+				<div class="small-6 medium-3 columns">
+					<img class="thumbnail" src="/img/our-story/gallery/our-story-8.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
+				</div>
+			</div>
+		</section>
+	<!-- </main> -->
+</div> <!-- end .off-canvas-content -->
