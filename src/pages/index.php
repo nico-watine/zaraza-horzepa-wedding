b507b211d2297afdca18c3da0a1b059ec35b118c
Nico Watine <nico@studioncreations.com>
2017-02-01 18:15:20 -0600

    New button on index to replace story
    Has messy SASS
    Clean that up

--- src/pages/index.php
+++ src/pages/index.php
@@ -61,15 +61,11 @@ body-class: index
 		</div>
 </div>
 		<div class="row">
-			<div class="medium-10 medium-push-1 columns end">
-				<article>
-					<p>Joe and Juliana have a love story sixteen years in the making. Their story started one hot and humid afternoon in Houston, Texas, on June 13, 2000, when Joe glanced down from his apartment window and noticed a group of women chatting and relaxing by the pool. One tall beautiful woman stood out from all the others and immediately captivated Joe. Joe made up his mind that he must meet this tall beautiful wonder. As he neared the group he could hear the women laughing and speaking to each other in Spanish. Not speaking any Spanish, Joe knew he had an uphill battle. Taking a deep breath, Joe looked straight at Juliana and addressed the group – “Hello, I’m Joe, can I interest anyone in drink?” Juliana listened, turned to her friends and quickly exchanged some comments in Spanish – comments that needed no translation - “Who is this guy, and why won’t he just leave us alone?” It was at that moment that both of their lives changed. Later that evening, Joe excitedly phoned a friend and informed him that he had just met his future wife – emphasis on “future” – it would prove to be a very long and rewarding journey up the hill.</p>
-					<p>For the next 10 years, on every June 13th, Joe ensured that Juliana received a little reminder of their first meeting. It did not matter if she was at home, or all the way in Colombia, visiting her family, flowers would always arrive on June 13th. The flowers led to the occasional dinner date at Sorrento’s restaurant, which led to regular conversations about life and politics, which led to a mutual respect and a deep and caring friendship. Finally, a decade after that fateful first step up the hill, Joe’s quiet patience and Juliana’s thoughtful compassion surprised both of them when they realized that slowly and thoughtfully each had been taking small loving steps towards each other.</p>
-					<p>On January 13, 2015, Joe proposed to Juliana at Sorrento’s opening a new and fulfilling chapter in their story. And so we ask you join us on another hot and humid afternoon in Cartagena to bear witness, celebrate and join in writing this new and exciting chapter in their story!</p>
-				</article>
+			<div class="medium-10 medium-push-1 columns end text-center">
+				<a class="button large" href="/our-story" style="margin-top: 16px;">The story continues...</a>
 			</div>
 		</div>
-		<section class="image-gallery">
+		<!-- <section class="image-gallery">
 			<div class="row">
 				<div class="small-6 medium-3 columns">
 					<img class="thumbnail" src="/img/our-story/gallery/our-story-1.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
@@ -98,6 +94,6 @@ body-class: index
 					<img class="thumbnail" src="/img/our-story/gallery/our-story-8.jpg" title="Joe &amp; Juliana" alt="Joe &amp; Juliana">
 				</div>
 			</div>
-		</section>
+		</section> -->
 	<!-- </main> -->
 </div> <!-- end .off-canvas-content -->
