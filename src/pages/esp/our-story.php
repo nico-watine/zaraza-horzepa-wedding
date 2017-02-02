bced61eba388180acae0814900e8e01e8ad2c661
Nico Watine <nico@studioncreations.com>
2017-02-01 18:23:09 -0600

    esp/our-story DONE
    Now fix the menu translation buttons

--- src/pages/esp/our-story.php
+++ src/pages/esp/our-story.php
@@ -5,7 +5,7 @@ description: The beautiful love story of Joe & Juliana, 16 years in the making.
 og-title: Joe & Juliana's Love Story
 og-description: Joe knew from the beginning that he had just met his future wife. A love story 16 years in the making.
 og-image:
-body-id: index
+body-id: our-story
 body-class: espanol
 ---
 <div class="off-canvas position-right" id="offCanvas" data-transition="overlap" data-off-canvas>
@@ -16,11 +16,11 @@ body-class: espanol
 
 	<!-- Menu -->
 	<ul class="vertical menu">
-		<li class="active"><a href="/esp">Nuestra Historia</a></li>
-		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/mx/tus-invitados/request-rsvp-by-guest?_ga=1.201991125.514128241.1485290807" target="_blank">RSVP</a></li>
-		<li><a href="/esp/wedding-details">Detalles de La Boda</a></li>
-		<li><a href="/esp/cartagena-info">Información de Cartagena</a></li>
-		<li><a href="/esp/registry">Lista de Boda</a></li>
+		<li class="active"><a href="/">Our Story</a></li>
+		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+		<li><a href="/wedding-details">Wedding Details</a></li>
+		<li><a href="/cartagena-info">Cartagena Info</a></li>
+		<li><a href="/registry">Registry</a></li>
 	</ul>
 </div>
 <div class="off-canvas-content" data-off-canvas-content style="background: none;">
@@ -39,11 +39,11 @@ body-class: espanol
 				</ul>
 				<nav class="mainNav text-right medium-text-left show-for-large">
 					<ul class="menu float-right">
-						<li class="active"><a style="width: 160px;" href="/esp">Nuestra Historia</a></li>
-						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/mx/tus-invitados/request-rsvp-by-guest?_ga=1.201991125.514128241.1485290807" target="_blank">RSVP</a></li>
-						<li><a style="width: 160px;" href="/esp/wedding-details">Detalles de La Boda</a></li>
-						<li><a style="width: 210px;" href="/esp/cartagena-info">Información de Cartagena</a></li>
-						<li><a style="width: 130px;" href="/esp/registry">Lista de Boda</a></li>
+						<li class="active"><a href="/">Our Story</a></li>
+						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/" target="_blank">RSVP</a></li>
+						<li><a href="/wedding-details">Wedding Details</a></li>
+						<li><a href="/cartagena-info">Cartagena Info</a></li>
+						<li><a href="/registry">Registry</a></li>
 					</ul>
 				</nav>
 			</div>
@@ -53,25 +53,30 @@ body-class: espanol
 		<div class="row">
 			<div class="medium-6 medium-push-6 columns">
 				<aside class="pageTitle">
-					<h1 class="script">"Acabo de conocer a mi futura esposa"</h1>
-					<p><i>- Joe Horzepa, le dijo a su mejor amigo por teléfono después de conocer a Juliana por primera vez <br>
-					- El 13 de Junio del año 2000</i></p>
+					<h1 class="script">Y la historia continúa..</h1>
 				</aside>
 			</div>
 		</div>
 </div>
-		<div class="row">
-			<div class="medium-10 medium-push-1 columns end">
-				<article>
+		<article>
+			<div class="row" id="first-row" style="margin-bottom: 30px;"">
+				<div class="medium-8 large-7 large-push-1 columns">
 					<p>Joe y Juliana tienen una historia de amor de dieciséis años. Su historia comenzó una tarde calurosa y húmeda en Houston, Texas, el 13 de Junio del año 2000, cuando Joe miró por la ventana de su apartamento y notó un grupo de mujeres charlando y relajándose en la piscina. Una mujer hermosa y alta se destacó de todas las demás e inmediatamente su belleza cautivó a Joe.</p>
 					<p>El decidió que debía conocer a esta hermosa mujer y al acercarse al grupo, pudo oír a las mujeres riéndose y hablando entre sí, en español. Sin el hablar este idioma, Joe sabía que tenía una difícil batalla. Respirando profundo, miró directamente a Juliana y se dirigió al grupo - "Hola, soy Joe, y les pregunto ¿será que alguien quiere una cerveza?" Juliana escuchó, se volvió a sus amigas y rápidamente intercambió algunos comentarios en español - comentarios que no necesitaban Traducción: "¿Quién es este tipo, y por qué no nos deja solas?" Fue en ese momento que ambas vidas cambiaron.</p>
+				</div>
+				<div class="medium-4 large-push-1 columns end text-center">
+					<img src="/img/our-story/ocean-view.jpg" alt="Joe &amp; Juliana" class="thumbnail">
+				</div>
+			</div>
+			<div class="row" id="second-row">
+				<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
 					<p>Más tarde esa noche, Joe con entusiasmo llamó a un amigo y le informó que acababa de conocer a su futura esposa - énfasis en el "futuro" - Ya que resultaría ser un viaje muy largo hasta que ambos pudieran tomar la decisión de estar juntos.</p>
 					<p>Durante los siguientes 10 años, cada 13 de junio, Joe se aseguró que Juliana recibiera un pequeño recordatorio de su primera reunión. No importaba si ella estaba en casa, la oficina, en Colombia visitando a su familia o donde estuviera... las flores llegarían siempre el 13 de junio (aquel grandioso 13 de junio).</p>
 					<p>Las flores llevaron a una cena casual en un restaurante llamado Sorrento, seguido por conversaciones sobre la vida y la política, lo que llevó a un respeto mutuo y una amistad profunda y cariñosa. Finalmente, una década después de aquel trascendente primer paso, la paciencia y determinación de Joe sorprendieron a Juliana. Ya que cuando se dieron cuenta, lentamente y cuidadosamente, cada uno había dado pequeños pasos amorosos … el uno hacia el otro.</p>
 					<p>El 13 de enero de 2015, Joe le propuso a Juliana, en Sorrento, la apertura de un nuevo y maravilloso capítulo en su historia. Así que les pedimos que se unan a nosotros en otra tarde calurosa, hermosa y llena de amor. En este paraíso, Cartagena, para dar testimonio, celebrar y unirse a la redacción de este nuevo y excitante capítulo en esta grandiosa historia de amor.</p>
-				</article>
+				</div>
 			</div>
-		</div>
+		</article>
 		<section class="image-gallery">
 			<div class="row">
 				<div class="small-6 medium-3 columns">
