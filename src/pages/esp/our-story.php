09e1199bb042472da4b0a0fc669c9377f26e94cd
Nico Watine <nico@studioncreations.com>
2017-08-03 13:36:28 -0500

    our-story in spanish done

--- src/pages/esp/our-story.php
+++ src/pages/esp/our-story.php
@@ -17,10 +17,10 @@ body-class: espanol
 	<!-- Menu -->
 	<ul class="vertical menu">
 		<li class="active"><a href="/esp">Nuestra Historia</a></li>
-		<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/mx/tus-invitados/request-rsvp-by-guest?_ga=1.201991125.514128241.1485290807" target="_blank">RSVP</a></li>
+		<!-- <li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/mx/tus-invitados/request-rsvp-by-guest?_ga=1.201991125.514128241.1485290807" target="_blank">RSVP</a></li> -->
 		<li><a href="/esp/wedding-details">Detalles de La Boda</a></li>
 		<li><a href="/esp/cartagena-info">Información de Cartagena</a></li>
-		<li><a href="/esp/registry">Lista de Boda</a></li>
+		<!-- <li><a href="/esp/registry">Lista de Boda</a></li> -->
 	</ul>
 </div>
 <div class="off-canvas-content" data-off-canvas-content style="background: none;">
@@ -40,10 +40,10 @@ body-class: espanol
 				<nav class="mainNav text-right medium-text-left show-for-large">
 					<ul class="menu float-right">
 						<li class="active"><a style="width: 160px;" href="/esp">Nuestra Historia</a></li>
-						<li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/mx/tus-invitados/request-rsvp-by-guest?_ga=1.201991125.514128241.1485290807" target="_blank">RSVP</a></li>
+						<!-- <li><a href="http://www.rsvp.zaraza-horzepa2017.wedding/mx/tus-invitados/request-rsvp-by-guest?_ga=1.201991125.514128241.1485290807" target="_blank">RSVP</a></li> -->
 						<li><a style="width: 160px;" href="/esp/wedding-details">Detalles de La Boda</a></li>
 						<li><a style="width: 210px;" href="/esp/cartagena-info">Información de Cartagena</a></li>
-						<li><a style="width: 130px;" href="/esp/registry">Lista de Boda</a></li>
+						<!-- <li><a style="width: 130px;" href="/esp/registry">Lista de Boda</a></li> -->
 					</ul>
 				</nav>
 			</div>
